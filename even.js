1213d91f79b8ca3e09d3487f409c2db66932b602
ramanmand <mandr@sheridancollege.ca>
2021-09-04 17:38:39 -0400

    isEven function file created

---
+++ even.js
@@ -0,0 +1,16 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+const isEvenn  = function (numn) {
+  return numn % 2 === 0;
+}
+
+console.log(isEvenn(10));
+console.log(isEvenn(11));
