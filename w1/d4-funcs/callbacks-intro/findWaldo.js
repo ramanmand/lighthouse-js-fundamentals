a78fa3c2324721c16781ab2ff60df013abcb1f8f
ramanmand <mandr@sheridancollege.ca>
2021-09-16 18:57:24 -0400

    callback function practise

---
+++ w1/d4-funcs/callbacks-intro/findWaldo.js
@@ -0,0 +1,15 @@
+const findWaldo = function(names, found) {
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    if (name === "Waldo") {
+      let index = i;
+      found(index);   // execute callback
+    }
+  }
+}
+
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}!`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
